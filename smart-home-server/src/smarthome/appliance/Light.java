02ba9f1367f099060b40efc76260d21212dee301
WallfacerRZD <1048058400@qq.com>
2018-06-11 18:02:34 +0800

    控制台灯

--- smart-home-server/src/smarthome/appliance/Light.java
+++ smart-home-server/src/smarthome/appliance/Light.java
@@ -1,11 +1,46 @@
 package smarthome.appliance;
 
-public class Light {
-	public void on() {
-		System.out.println("the light on");
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-	public void off() {
-		System.out.println("the light off");
-	}
+public class Light {
+    private static final String LIGHT_ON_URL = "http://192.168.23.2/on";
+
+    private static final String LIGHT_OFF_URL = "http://192.168.23.2/off";
+
+    public void on() {
+        request(LIGHT_ON_URL);
+    }
+
+    public void off() {
+        request(LIGHT_OFF_URL);
+    }
+
+    public static void main(String[] args) throws MalformedURLException {
+        new Light().on();
+        new Light().off();
+    }
+
+    private void request(final String url) {
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            // NodeMUC的response不符合标准, 直接忽略(逃..
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
 }
